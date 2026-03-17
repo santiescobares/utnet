51233d8d21dd90d64db29372521f1fe90032c2e3
Santiago Escobares <santiagoescobares06@gmail.com>
2026-03-17 00:48:16 -0300

    Initial commit

---
+++ backend/src/test/java/ar/net/ut/backend/BootstrapTests.java
@@ -0,0 +1,13 @@
+package ar.net.ut.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
