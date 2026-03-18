8f325cfa68e594bf4a37efc487ad0b0afe3afed7
Santiago Escobares <santiagoescobares06@gmail.com>
2026-03-18 18:28:06 -0300

    feat: create global variables

--- backend/src/main/java/ar/net/ut/backend/Global.java
+++ backend/src/main/java/ar/net/ut/backend/Global.java
@@ -1,5 +1,6 @@
 package ar.net.ut.backend;
 
+import lombok.AllArgsConstructor;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -7,4 +8,32 @@ public final class Global {
 
     public final String API_VERSION_PATH = "/v1";
     public final String APP_URL = "https://ut.net.ar";
+
+    public final String ACCESS_TOKEN_COOKIE = "access_token";
+
+    @AllArgsConstructor
+    public enum R2 {
+        PUBLIC_URL(""),
+        PROFILE_PICTURES_PATH("profile-pictures/");
+
+        final String value;
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+    @AllArgsConstructor
+    public enum RedisKeys {
+        TOKEN_BLACKLIST("token_blacklist:"),
+        FORCED_LOGOUT("forced_logout:");
+
+        final String value;
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
 }
